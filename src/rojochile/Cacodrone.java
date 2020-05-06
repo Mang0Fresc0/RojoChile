6331278b0b0909645614e18c511a42628bd6aa15
Misael <Misael@LAPTOP-P29GALO2>
2020-05-06 13:49:50 -0700

    El mapa ya se genera como pasto al azar de un tamaño definido, la clase cacodrone tiene cierto esqueleto.

--- src/rojochile/Cacodrone.java
+++ src/rojochile/Cacodrone.java
@@ -1,13 +1,31 @@
 package rojochile;
 
+import java.util.ArrayList;
 import javax.swing.ImageIcon;
 
 public class Cacodrone extends Mob {
+
+    ArrayList<Bala> toDispose = new ArrayList<>();
+    ArrayList<Bala> balas = new ArrayList<>();
+    Bala bullet;
 
     public Cacodrone(int x, int y) {
         this.x = x;
         this.y = y;
         move = new ImageIcon(this.getClass().getResource("Animaciones/Cacodrone/cacodrone.gif")).getImage();
         dead = new ImageIcon(this.getClass().getResource("Animaciones/Cacodrone/cacodroneDead.gif")).getImage();
+    }
+
+    @Override
+    public void attack() {
+        if (windUp < STDATKT) {
+            windUp += agility;
+        } else if (windUp >= STDATKT) {
+            shoot();
+        }
+    }
+
+    public void shoot() {
+        balas.add(bullet);
     }
 }
--- src/rojochile/Map.java
+++ src/rojochile/Map.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Random;
 import javax.imageio.ImageIO;
 
 public class Map {
@@ -16,8 +17,8 @@ public class Map {
     Tile tiles[][];
     String type[][];
     HashMap<String, BufferedImage> images = new HashMap<>();
+    Random r = new Random();
     private static final GraphicsConfiguration GFX_CONFIG = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
-
 
     public Map(int width, int height) throws IOException {
         type = new String[width][height];
@@ -28,11 +29,34 @@ public class Map {
 
     private void setMap(int x, int y) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("Resources/maps/2.txt"));
-        String line;
+        //String line;
         for (int i = 0; i < y; i++) {
-            line = reader.readLine();
-            for (int j = 0; j < 2*x; j+=2) {
-            type[j/2][i] = String.format("%c%c", line.charAt(j), line.charAt(j+1));
+            //line = reader.readLine();
+            for (int j = 0; j < 2 * x; j += 2) {
+                switch (r.nextInt(7)) {
+                    case 1:
+                        type[j / 2][i] = "b1";
+                        break;
+                    case 2:
+                        type[j / 2][i] = "g1";
+                        break;
+                    case 3:
+                        type[j / 2][i] = "r1";
+                        break;
+                    case 4:
+                        type[j / 2][i] = "b1";
+                        break;
+                    case 5:
+                        type[j / 2][i] = "g1";
+                        break;
+                    case 6:
+                        type[j / 2][i] = "r1";
+                        break;
+                    default:
+                        type[j / 2][i] = "y1";
+                        break;
+                }
+                //type[j / 2][i] = String.format("%c%c", line.charAt(j), line.charAt(j + 1));
             }
         }
         for (int i = 0; i < y; i++) {
@@ -49,14 +73,14 @@ public class Map {
             }
         }
     }
-    
-    private void setImages() throws IOException{
-    images.put("r1", toCompatibleImage(ImageIO.read(new File("Resources/tiles/r1.png"))));
-    images.put("b1", toCompatibleImage(ImageIO.read(new File("Resources/tiles/b1.png"))));
-    images.put("g1", toCompatibleImage(ImageIO.read(new File("Resources/tiles/g1.png"))));
-    images.put("y1", toCompatibleImage(ImageIO.read(new File("Resources/tiles/y1.png"))));
+
+    private void setImages() throws IOException {
+        images.put("r1", toCompatibleImage(ImageIO.read(new File("Resources/tiles/r1.png"))));
+        images.put("b1", toCompatibleImage(ImageIO.read(new File("Resources/tiles/b1.png"))));
+        images.put("g1", toCompatibleImage(ImageIO.read(new File("Resources/tiles/g1.png"))));
+        images.put("y1", toCompatibleImage(ImageIO.read(new File("Resources/tiles/y1.png"))));
     }
-    
+
     //nada que ver aquí, solo código robado
     public static BufferedImage toCompatibleImage(final BufferedImage image) {
         /*
